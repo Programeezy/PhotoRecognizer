b7448af17914c99749db4147f1c0032f5fd4692f
Tim Smolonski <programeezy@gmail.com>
2019-12-09 14:26:03 +0300

    Refactor code

--- photo_backend/photo_app/views.py
+++ photo_backend/photo_app/views.py
@@ -1,11 +1,7 @@
-import codecs
-
-import face_recognition
-import numpy as np
 import pickle
 
+import face_recognition
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from photo_app.models import Person
@@ -13,12 +9,11 @@ from photo_app.models import Person
 
 @csrf_exempt
 def upload_picture(request):
-    print(request)
     image = face_recognition.load_image_file(request.FILES['photo'])
     known_faces = [(l[0], pickle.loads(l[1])) for l in Person.objects.values_list('name', 'photo_encoding')]
     faces = [l[1] for l in known_faces]
     uploaded_face = face_recognition.face_encodings(image)[0]
-    result = face_recognition.compare_faces(faces, uploaded_face, tolerance=0.4)
+    result = face_recognition.compare_faces(faces, uploaded_face, tolerance=0.6)
     try:
         match = result.index(True)
     except:
